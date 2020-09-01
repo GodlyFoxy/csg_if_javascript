fd08ddae8c93a95145394600ddceb29c04b003a3
GodlyFoxy <godlyfoxxy@gmail.com>
2020-09-01 18:26:57 +0000

    sdfsd

--- JS/H2/H2O13.js
+++ JS/H2/H2O13.js
@@ -1,5 +1,5 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
+var aantalRijenRaster = 12;
+var aantalKolommenRaster = 18;
 var celGrootte;
 
 var animatie = [];
@@ -11,6 +11,11 @@ var xJos = 400;
 var yJos = 300;
 
 function preload() {
+  for(i = 0; i<aantalBeeldjes;i++)
+  {
+      newImage = loadImage("images/sprites/Jos100px/Jos_" + i + ".png");
+      animatie.push(newImage);
+  }  
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
   frame = loadImage("images/sprites/Jos100px/Jos_0.png");
 }
@@ -28,21 +33,25 @@ function draw() {
 
   if (keyIsDown(LEFT_ARROW)) {
     xJos -= celGrootte;
+    nummer = 2;
   }
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
+    nummer = 1;
   }
   if (keyIsDown(UP_ARROW)) {
     yJos-=celGrootte;
+    nummer = 4;
   }
   if (keyIsDown(DOWN_ARROW)) {
     yJos += celGrootte;
+    nummer = 5;
   }
   
   xJos = constrain(xJos,0,width - celGrootte);
   yJos = constrain(yJos,0,height - celGrootte);
   
-  image(frame,xJos,yJos);
+  image(animatie[nummer],xJos,yJos,0.5 * animatie[nummer].width, 0.5 * animatie[nummer].height);
 }
 
 function tekenRaster() {
